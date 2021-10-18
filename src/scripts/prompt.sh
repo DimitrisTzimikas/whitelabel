9e3287e56584f80104642049aaacf308d087eea8
Dimitris Tzimikas <dimitris92a@hotmail.com>
2021-10-18 22:11:45 +0300

    Create prompt.sh

---
+++ src/scripts/prompt.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#######################################################################
+# prompt script takes $1 as argument and displays the question to user
+# then user needs to answer with y or n
+#######################################################################
+
+function prompt() {
+  local QUESTION=$1
+
+  while true; do
+    read -r -p "$QUESTION [y/n]: " -n 1
+    echo
+
+    if [[ $REPLY == y ]]; then
+      return 0
+    elif [[ $REPLY == n ]]; then
+      return 1
+    else
+      echo "Please give answer of y or n."
+    fi
+  done
+}
